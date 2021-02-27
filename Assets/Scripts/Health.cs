dbe0ed821ac69884cf52263ca6655a0310527eef
Mihai Andrei Popa <popamihai18@yahoo.com>
2021-02-27 12:09:10 +0200

    Small health system script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 100f;
+
+    private float currentHealth;
+
+    private void Start()
+    {
+        this.currentHealth = this.maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        this.currentHealth -= damage;
+
+        if (this.currentHealth <= 0)
+        {
+            this.Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
