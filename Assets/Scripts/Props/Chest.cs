87e04b6bc0786ee51432603b0fd38038496a649a
Condor Andrei <44895249+condoran@users.noreply.github.com>
2021-02-28 22:25:52 +0200

    EZ First try healing and chest

---
+++ Assets/Scripts/Props/Chest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public Transform item;
+    public Transform spawnPosition;
+
+    private bool interacted;
+
+    private void Start()
+    {
+        interacted = false;
+    }
+
+
+    public void Interact()
+    {
+        if (!interacted)
+        { 
+            Instantiate(item, spawnPosition.position, Quaternion.identity);
+            interacted = true;
+        }
+    }
+}
