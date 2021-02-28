0fc1b4bef7206b155e1d9536f5c005d43cb20e96
Mihai Andrei Popa <popamihai18@yahoo.com>
2021-02-28 23:24:58 +0200

    Attack & defense finished with "animations" and delay

---
+++ Assets/Scripts/Props/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private void Update()
+    {
+        if (this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
