6cf21f323dcd5b4c19e8ce90436e9ac156eae228
Condor Andrei <44895249+condoran@users.noreply.github.com>
2021-03-01 01:40:08 +0200

    Fixed it, Fk u git!

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    private bool gamePaused;
+
+    void Start()
+    {
+        gamePaused = false;
+    }
+
+    public void Action()
+    {
+        if (gamePaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Pause()
+    {
+        gamePaused = true;
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        gamePaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
